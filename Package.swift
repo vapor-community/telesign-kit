68b977aa13291618e219da145b6ce1e5ef1c1090
Chad <geeksweep@gmail.com>
2026-02-27 22:59:34 -0500

    Vapor4Update
    - manually adding in swift crypto and removing package.resolved.

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-crypto.git", from: "1.0.0")
+        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "5.0.0")
     ],
     targets: [
         .target(name: "TelesignKit", dependencies: [
